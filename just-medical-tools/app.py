ede44963babb4cb3b9ad49720d2286aafbfb2bca
Jolo Villanueva <el.gamer@yahoo.com.ph>
2022-06-20 20:25:35 +0800

    First commit

---
+++ just-medical-tools/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_url_path='')
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/bmi", methods=['GET','POST'])
+def bmi():
+    return render_template('bmi.html')
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
