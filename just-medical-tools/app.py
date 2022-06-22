ca18f6783b59eb07cd36c20c90dd98ae41608477
Jolo Villanueva <el.gamer@yahoo.com.ph>
2022-06-22 16:14:18 +0800

    Second commit with working BMI Calculator

--- just-medical-tools/app.py
+++ just-medical-tools/app.py
@@ -1,6 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from flask_wtf import FlaskForm
+from wtforms import DecimalField, SubmitField
+from wtforms.validators import DataRequired
+import os
 
 app = Flask(__name__, static_url_path='')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+
+class BMIForm(FlaskForm):
+    weight = DecimalField("Weight in kilograms:", validators=[DataRequired()])
+    height = DecimalField("Height in meters:", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
 
 @app.route("/")
 def index():
@@ -8,9 +19,19 @@ def index():
 
 @app.route("/bmi", methods=['GET','POST'])
 def bmi():
-    return render_template('bmi.html')
-
+    weight=None
+    height=None
+    bmi=None
+    formatted_bmi=None
+    form=BMIForm()
+    if form.validate_on_submit():
+        print("form validated")
+        weight=form.weight.data
+        height=form.height.data
+        bmi=weight/height**2
+        formatted_bmi="{:.2f}".format(bmi)
+    return render_template('bmi.html', bmi=formatted_bmi, form=form)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
